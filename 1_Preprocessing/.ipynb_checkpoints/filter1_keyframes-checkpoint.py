48d4d53dff00ea2d6b26dc8e14b09ecc812ea7c3
Addison Sandvik <addisonsandvik@gmail.com>
2024-05-21 17:30:20 -0700

    Reorganize

---
+++ 1_Preprocessing/.ipynb_checkpoints/filter1_keyframes-checkpoint.py
@@ -0,0 +1,20 @@
+"""
+This script removes all non-keyframe boxes from the
+CVAT's raw annotation file, resulting in a much
+smaller file size without loss of relevent 
+information.
+"""
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('annotations.xml')
+root = tree.getroot()
+
+for track in root.iter('track'):
+    i = 0
+    while i < len(track):
+        if (track[i].attrib.get('keyframe') == "0"):
+            track.remove(track[i])
+        else:
+            i += 1
+
+tree.write('annotations1_keyframes.xml')
