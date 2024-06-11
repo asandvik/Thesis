1fd9815e2d8b30ad7b0dfb696ee1644ffb76ba10
Addison Sandvik <addisonsandvik@gmail.com>
2024-06-10 21:21:13 -0700

    network diagram

--- 2_Training/trainLoop.py
+++ 2_Training/trainLoop.py
@@ -14,13 +14,14 @@ from CPTADDataset import CPTADDataset
 from datetime import datetime
 from torch.utils.tensorboard import SummaryWriter
 from transforms import ConvertBCHWtoCBHW
-from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score, accuracy_score
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--learn', type=float, default=0.001, help='learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--batch', type=int, default=16, help='batch size')
+parser.add_argument('--workers', type=int, default=4, help='number of dataloader workers')
 parser.add_argument('--check', type=int, default=10, help='how many batches before sub-epoch reporting')
 parser.add_argument('--epochs', type=int, default=10, help='maximum number of epochs')
 parser.add_argument('--chkpnt', type=int, default=5, help='number of epochs before checkpointing')
@@ -30,6 +31,8 @@ parser.add_argument('--model', type=str, default='cnn', const='cnn', nargs='?',
                     choices=['cnn','swin'], help='R(2+1)D or Swin Transformer')
 parser.add_argument('--loader', type=str, default='intervals', const='intervals', nargs='?',
                     choices=['intervals','windows'], help='data loader sampling technique')
+parser.add_argument('--loss', type=str, default='CE', const='CE', nargs='?',
+                    choices=['CE','BCE'], help='loss function')
 parser.add_argument('tag', type=str, help='unique tag for this experiment')
 
 args = parser.parse_args()
@@ -46,23 +49,27 @@ print(f"Using {device} device")
 
 if args.model == 'cnn':
     model = r2plus1d_18(weights=R2Plus1D_18_Weights.DEFAULT)
-    for param in model.parameters(): # use as fixed feature extractor
-        param.requires_grad = False
+    # for param in model.parameters(): # use as fixed feature extractor
+    #     param.requires_grad = False
 
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, 2)
 
     model = model.to(device)
-
-    # criterion = torch.nn.BCEWithLogitsLoss()
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.fc.parameters(), lr=args.learn, momentum=args.momentum)
     
 elif args.model == 'swin':
     model = swin3d_b(weights=Swin3D_B_Weights.DEFAULT).to(device)
 else:
     error('invalid model. argparse should have caught.')
 
+if args.loss == 'CE':
+    criterion = torch.nn.BCEWithLogitsLoss()
+elif args.loss == 'BCE':
+    criterion = torch.nn.CrossEntropyLoss()
+else:
+    error('invalid loss function. argparse should have caught.')
+    
+optimizer = torch.optim.SGD(model.parameters(), lr=args.learn, momentum=args.momentum)
 
 # https://pytorch.org/vision/0.8/models.html#video-classification
 mean = [0.43216, 0.394666, 0.37645]
@@ -110,13 +117,14 @@ test_data = CPTADDataset(ANNO_DIR + "anno_test.csv",
 
 print("NUM SAMPLES train:{} validation:{} test:{}".format(len(training_data), len(valid_data), len(test_data)))
 
-train_dataloader = DataLoader(training_data, batch_size=args.batch)
-valid_dataloader = DataLoader(valid_data, batch_size=args.batch)
-test_dataloader = DataLoader(test_data, batch_size=args.batch)
+train_dataloader = DataLoader(training_data, batch_size=args.batch, num_workers=args.workers)
+valid_dataloader = DataLoader(valid_data, batch_size=args.batch, num_workers=args.workers)
+test_dataloader = DataLoader(test_data, batch_size=args.batch, num_workers=args.workers)
 
 writer = SummaryWriter(RUNS_DIR)
 
-val_f1_best = 0
+val_accuracy_best = 0
+val_recall_best = 0
 
 for epoch in range(args.epochs):
     print('Epoch-{0} lr: {1}'.format(epoch+1, optimizer.param_groups[0]['lr']))
@@ -150,10 +158,11 @@ for epoch in range(args.epochs):
         batch_loss = loss.item() # average batch loss
         train_loss += batch_loss*inputs.size(0) # total loss for batch
 
-        if i % args.check == args.check-1:
-            # print('  batch {} loss: {}'.format(i, batch_loss))
-            tb_x = epoch * len(train_dataloader) + i + 1
-            writer.add_scalars(tag + '/BatchLoss', {'Epoch_'+str(epoch+1):batch_loss}, tb_x)
+        # if i % args.check == args.check-1:
+        #     # print('  batch {} loss: {}'.format(i, batch_loss))
+        #     tb_x = epoch * len(train_dataloader) + i + 1
+        #     writer.add_scalars(tag + '/BatchLoss', {'Epoch_'+str(epoch+1):batch_loss}, tb_x)
+        #     writer.flush()
             
     # End of epoch
     
@@ -192,13 +201,16 @@ for epoch in range(args.epochs):
     train_recall = recall_score(train_true, train_pred)
     val_recall = recall_score(val_true, val_pred)
 
+    train_accuracy = accuracy_score(train_true, train_pred)
+    val_accuracy = accuracy_score(val_true, val_pred)
+
     # print('F1 SCORE train {} val {}'.format(train_f1,val_f1))
 
     # constant for classes
-    classes = ('No Crash', 'Crash')
+    # classes = ('No Crash', 'Crash')
     
     # confusion matrix
-    # cf_matrix = confusion_matrix(val_true, val_pred)
+    val_cf_matrix = confusion_matrix(val_true, val_pred)
     # df_cm = pd.DataFrame(cf_matrix / np.sum(cf_matrix, axis=1)[:, None], index=[i for i in classes],
     #                      columns=[i for i in classes])
     # plt.figure(figsize=(12, 7))
@@ -206,7 +218,7 @@ for epoch in range(args.epochs):
     
     # writer.add_figure(tag + '/Validation Confusion matrix', cm_fig, epoch)
 
-    # cf_matrix = confusion_matrix(train_true, train_pred)
+    train_cf_matrix = confusion_matrix(train_true, train_pred)
     # df_cm = pd.DataFrame(cf_matrix / np.sum(cf_matrix, axis=1)[:, None], index=[i for i in classes],
     #                      columns=[i for i in classes])
     # plt.figure(figsize=(12, 7))
@@ -222,6 +234,10 @@ for epoch in range(args.epochs):
                     {'Training':train_ave_loss,'Validation':val_ave_loss},
                     epoch+1)
 
+    writer.add_scalars(tag + '/Training vs. Validation Accuracy',
+                {'Training':train_accuracy,'Validation':val_accuracy},
+                epoch+1)
+
     writer.add_scalars(tag + '/Training vs. Validation Recall',
                 {'Training':train_recall,'Validation':val_recall},
                 epoch+1)
@@ -229,19 +245,40 @@ for epoch in range(args.epochs):
     writer.add_scalars(tag + '/Training vs. Validation Precision',
                     {'Training':train_precision,'Validation':val_precision},
                     epoch+1)
+
+    # Double check labels are correct with recall/precision/accuracy calculation
+    writer.add_scalars(tag + '/Training TFPN',
+                      {'TN':train_cf_matrix[0][0],'FP':train_cf_matrix[0][1],
+                       'FN':train_cf_matrix[1][0],'TP':train_cf_matrix[1][1]},
+                      epoch+1)
+
+    writer.add_scalars(tag + '/Validation TFPN',
+                  {'TN':val_cf_matrix[0][0],'FP':val_cf_matrix[0][1],
+                   'FN':val_cf_matrix[1][0],'TP':val_cf_matrix[1][1]},
+                  epoch+1)
     writer.flush()
 
     # checkpoint
-    if epoch+1 % args.chkpnt == 0:
-        model_path = '{}{}_checkpoint_{}'.format(MODEL_DIR,tag,epoch+1)
+    if (epoch+1) % args.chkpnt == 0:
+        model_path = '{}{}_checkpoint'.format(MODEL_DIR,tag)
         print("Checkpointing model. Epoch: {}".format(epoch+1))
         torch.save(model.state_dict(), model_path)
 
-    # save best model
-    if val_f1 > val_f1_best:
-        val_f1_best = val_f1
-        model_path = '{}{}_best'.format(MODEL_DIR,tag)
+    # save best accuracy
+    if val_accuracy > val_accuracy_best:
+        val_accuracy_best = val_accuracy
+        model_path = '{}{}_bestAccuracy'.format(MODEL_DIR,tag)
         print("Saving model. Epoch: {}".format(epoch+1))
+        best_accuracy_epoch = epoch+1
         torch.save(model.state_dict(), model_path)
 
+    # save best recall
+    if val_recall > val_recall_best:
+        val_recall_best = val_recall
+        model_path = '{}{}_bestRecall'.format(MODEL_DIR,tag)
+        print("Saving model. Epoch: {}".format(epoch+1))
+        best_recall_epoch = epoch+1
+        torch.save(model.state_dict(), model_path)
 
+print("Best Accuracy Epoch: {}".format(best_accuracy_epoch))
+print("Best Recall Epoch: {}".format(best_recall_epoch))
